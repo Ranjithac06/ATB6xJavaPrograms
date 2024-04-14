82998b42dbb71cfe3864c7eae05785d8accc3f86
Ranjitha <cranjitha776@gmail.com>
2024-04-14 12:10:47 +0530

    operators and literals

--- src/ClassName/Operators.java
+++ src/ClassName/Operators.java
@@ -1,2 +1,14 @@
-package ClassName;public class Operators {
+package ClassName;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a= 33;
+        int b = 10;
+        int c = 20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(c);
+    }
 }
--- src/ClassName/literals.java
+++ src/ClassName/literals.java
@@ -1,2 +1,10 @@
-package ClassName;public class lierals {
+package ClassName;
+
+public class literals {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.7f;
+        double c = 8.9d;
+        System.out.println(a+"," + b + ", " + c);
+    }
 }
