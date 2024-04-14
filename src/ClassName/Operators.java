a472ef9b7fe57d101bcdbb3e9fa88706a2a638ac
Ranjitha <cranjitha776@gmail.com>
2024-04-14 12:14:32 +0530

    operators and literals

--- src/ClassName/Operators.java
+++ src/ClassName/Operators.java
@@ -4,11 +4,10 @@ public class Operators {
     public static void main(String[] args) {
         int a= 33;
         int b = 10;
-        int c = 20;
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a/b);
         System.out.println(a%b);
-        System.out.println(c);
+
     }
 }
