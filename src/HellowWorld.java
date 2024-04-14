be4b8714c919ba5c2023ccc1518c194d892fc588
Chakali Ranjitha <ranjitha_c@blr.trigent.com>
2024-04-14 10:18:02 +0530

    helloworld

---
+++ src/HellowWorld.java
@@ -0,0 +1,6 @@
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("HI Krishna for helping me out for git push");
+        System.out.println("Hello World");
+    }
+}
